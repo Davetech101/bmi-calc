788431d46bcbb90110618f2e7a19daf5f3f659a8
Ogaranya <enomahog@gmail.com>
2023-08-15 12:15:55 +0100

    first commit

---
+++ app/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Header(): React.JSX.Element {
+    return (
+        <header className='bg-gray-800 text-slate-50 h-20'>
+            header
+        </header>
+    )
+}
+
+export default Header
