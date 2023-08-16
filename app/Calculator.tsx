695d790b8fa0b815b1c889478ca47874dd3cf510
Ogaranya <enomahog@gmail.com>
2023-08-16 13:48:24 +0100

    height

--- app/Calculator.tsx
+++ app/Calculator.tsx
@@ -4,6 +4,7 @@ import { BsGenderMale, BsGenderFemale } from "react-icons/bs";
 function Calculator(): React.JSX.Element {
   const genderStyles =
     "text-3xl text-gray-300 p-10 bg-dark2 rounded-lg w-full flex flex-col items-center justify-center";
+    const heightStyles = ""
   return (
     <main className="p-10">
       <div className="flex items-center justify-between gap-5">
@@ -16,6 +17,19 @@ function Calculator(): React.JSX.Element {
           <BsGenderFemale size="8rem" />
           <p className="mt-7">Female</p>
         </button>
+      </div>
+
+      <div className="mt-7 text-gray-300">
+        <button className="text-2xl">CM</button>
+        <button className="text-2xl">FT</button>
+        <div className="">
+          <input type="number" placeholder="cm"/>
+        </div>
+
+        <div className="">
+            <input type="number" className="ft" />
+            <input type="number" className="inches" />
+        </div>
       </div>
     </main>
   );
