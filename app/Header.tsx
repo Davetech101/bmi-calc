7c731ab06f0fafd242b9dc3b6de3d18e8d697e6d
Ogaranya <enomahog@gmail.com>
2023-08-20 05:55:00 +0100

    header

--- app/Header.tsx
+++ app/Header.tsx
@@ -3,7 +3,7 @@ import { RxHamburgerMenu } from "react-icons/rx";
 
 function Header(): React.JSX.Element {
   return (
-    <header className="bg-dark  text-gray-300 h-28 flex items-center p-10 justify-between shadow-xl">
+    <header className="bg-dark fixed top-0 left-0 w-screen text-gray-300 h-28 flex items-center p-10 justify-between shadow-xl">
       <button className="">
         <RxHamburgerMenu size="3rem" />
       </button>
